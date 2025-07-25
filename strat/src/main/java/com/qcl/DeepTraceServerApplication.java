915545e9eb82d17760ea5b72ddf9a9060eca259e
JasonHans <2524126245@qq.com>
2025-07-25 16:45:50 +0800

    规范项目结构

--- strat/src/main/java/com/qcl/DeepTraceServerApplication.java
+++ strat/src/main/java/com/qcl/DeepTraceServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 入口类=
+ * 入口类
  */
 @SpringBootApplication
 public class DeepTraceServerApplication {
