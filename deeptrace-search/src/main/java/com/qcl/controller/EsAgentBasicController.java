b1dc8822d35b975e466050802bcb72c72bd41c4b
tbyno1 <m17864197141_1@163.com>
2025-08-06 10:20:34 +0800

    采集器下，列表展示页的分页和关键字过滤支持

--- deeptrace-search/src/main/java/com/qcl/controller/EsAgentBasicController.java
+++ deeptrace-search/src/main/java/com/qcl/controller/EsAgentBasicController.java
@@ -1,9 +1,9 @@
 package com.qcl.controller;
 
-
 import com.qcl.entity.AgentBasic;
 import com.qcl.service.EsAgentBasicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page; // 加上这一行
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +22,9 @@ public class EsAgentBasicController {
     public ResponseEntity<List<AgentBasic>> search(@RequestParam(required = false) String keyword,
                                                    @RequestParam(required = false, defaultValue = "0") Integer pageNum,
                                                    @RequestParam(required = false, defaultValue = "5") Integer pageSize) {
-        List<AgentBasic> agentLog = esAgentBasicService.search(keyword, pageNum, pageSize);
-        return ResponseEntity.ok(agentLog);
+        // 获取分页结果
+        Page<AgentBasic> page = esAgentBasicService.search(keyword, pageNum, pageSize);
+        // 只返回内容列表
+        return ResponseEntity.ok(page.getContent());
     }
 }
--- deeptrace-search/src/main/java/com/qcl/service/EsAgentBasicService.java
+++ deeptrace-search/src/main/java/com/qcl/service/EsAgentBasicService.java
@@ -1,10 +1,10 @@
 package com.qcl.service;
 
 import com.qcl.entity.AgentBasic;
-import com.qcl.entity.AgentLog;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
+//import java.util.List;
 
 public interface EsAgentBasicService {
-    List<AgentBasic> search(String keyword, Integer pageNum, Integer pageSize);
+    Page<AgentBasic> search(String keyword, Integer pageNum, Integer pageSize);
 }
