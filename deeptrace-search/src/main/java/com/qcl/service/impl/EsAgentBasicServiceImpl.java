dc889bdcdf83fb23380d0889c7a4bf7ca2180841
tbyno1 <m17864197141_1@163.com>
2025-08-06 10:20:34 +0800

    采集器下，列表展示页的分页和关键字过滤支持

--- deeptrace-search/src/main/java/com/qcl/service/impl/EsAgentBasicServiceImpl.java
+++ deeptrace-search/src/main/java/com/qcl/service/impl/EsAgentBasicServiceImpl.java
@@ -1,28 +1,32 @@
 package com.qcl.service.impl;
 
 import com.qcl.entity.AgentBasic;
-import com.qcl.entity.AgentLog;
+//import com.qcl.entity.AgentLog;
 import com.qcl.repository.EsAgentBasicRepository;
-import com.qcl.repository.EsAgentLogRepository;
+//import com.qcl.repository.EsAgentLogRepository;
 import com.qcl.service.EsAgentBasicService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//import java.util.stream.StreamSupport;
 
 @Service
 public class EsAgentBasicServiceImpl implements EsAgentBasicService {
     @Autowired
     private EsAgentBasicRepository esAgentBasicRepository;
 
-
     @Override
-    public List<AgentBasic> search(String keyword, Integer pageNum, Integer pageSize) {
-        Iterable<AgentBasic > iterable = esAgentBasicRepository.findAll();
-        List<AgentBasic> result = StreamSupport.stream(iterable.spliterator(), false)
-                .collect(Collectors.toList());
-        return result;
+    public Page<AgentBasic> search(String keyword, Integer pageNum, Integer pageSize) {
+        // 分页请求
+        PageRequest pageRequest = PageRequest.of(pageNum, pageSize);
+        // keyword 查询
+        if (keyword == null || keyword.isEmpty()) {
+            return esAgentBasicRepository.findAll(pageRequest);
+        }
+        return esAgentBasicRepository.findByNameContaining(keyword, pageRequest);
     }
 }
