5062080e0222faef97fd9f13e7044168ae93524b
tbyno1 <m17864197141_1@163.com>
2025-08-11 10:22:02 +0800

    跨域配置

--- deeptrace-search/src/main/java/com/qcl/vo/PageResult.java
+++ deeptrace-search/src/main/java/com/qcl/vo/PageResult.java
@@ -1,7 +1,12 @@
 package com.qcl.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
+@Setter
+@Getter
 public class PageResult<T> {
     private List<T> content;
     private int pageNumber;
@@ -17,14 +22,4 @@ public class PageResult<T> {
         this.totalPages = totalPages;
     }
 
-    public List<T> getContent() { return content; }
-    public void setContent(List<T> content) { this.content = content; }
-    public int getPageNumber() { return pageNumber; }
-    public void setPageNumber(int pageNumber) { this.pageNumber = pageNumber; }
-    public int getPageSize() { return pageSize; }
-    public void setPageSize(int pageSize) { this.pageSize = pageSize; }
-    public long getTotalElements() { return totalElements; }
-    public void setTotalElements(long totalElements) { this.totalElements = totalElements; }
-    public int getTotalPages() { return totalPages; }
-    public void setTotalPages(int totalPages) { this.totalPages = totalPages; }
 }
