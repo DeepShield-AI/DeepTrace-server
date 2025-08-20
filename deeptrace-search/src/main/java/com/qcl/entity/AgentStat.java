b0d0d554c54401a7aa85032ae00a5fd1c1343046
tbyno1 <m17864197141_1@163.com>
2025-08-20 15:16:13 +0800

    监控信息数据结构改变，按时间段返回内存和cpu两个对象数组

--- deeptrace-search/src/main/java/com/qcl/entity/AgentStat.java
+++ deeptrace-search/src/main/java/com/qcl/entity/AgentStat.java
@@ -1,6 +1,5 @@
 package com.qcl.entity;
 
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Id;
@@ -8,26 +7,37 @@ import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 
 import java.io.Serializable;
-import java.util.Date;
 import java.util.List;
 
+/**
+ * 监控信息实体类
+ */
 @Data
 @EqualsAndHashCode
 @Document(indexName = "agent_stats")
 public class AgentStat implements Serializable {
     private static final long serialVersionUID = -1L;
 
+    // 名称（主键）
     @Id
     @Field(name = "agent_name")
     private String agentName;
+
+    // 进程ID
+    private String pid;
+
+    // 时间戳列表
+    private List<String> timestamps;
+
+    // CPU使用率
     @Field(name = "cpu_usages")
     private List<Float> cpuUsages;
+
+    // 内存使用率
     @Field(name = "memory_usages")
     private List<Float> memoryUsages;
 
-    private String pid;
+    // span数量
     @Field(name = "span_nums")
     private List<Long> spanNums;
-
-    private List<String> timestamps;
 }
