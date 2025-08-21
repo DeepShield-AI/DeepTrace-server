0baaba2397af6da8c4db78a6fc2249f0af1b8dee
tbyno1 <m17864197141_1@163.com>
2025-08-21 15:57:06 +0800

    查询列表单条数据详情

--- deeptrace-search/src/main/java/com/qcl/service/EsAgentBasicService.java
+++ deeptrace-search/src/main/java/com/qcl/service/EsAgentBasicService.java
@@ -3,8 +3,9 @@ package com.qcl.service;
 import com.qcl.entity.AgentBasic;
 import org.springframework.data.domain.Page;
 
-//import java.util.List;
-
 public interface EsAgentBasicService {
+    // 分页查询
     Page<AgentBasic> search(String keyword, Integer pageNum, Integer pageSize);
+    // 单条
+    AgentBasic findByLcuuid(String lcuuid);
 }
