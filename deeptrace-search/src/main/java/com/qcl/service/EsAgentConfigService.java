eec37fcfdbac8edf59e341189b0cb4b6af0c71a4
tbyno1 <m17864197141_1@163.com>
2025-08-21 15:57:06 +0800

    查询列表单条数据详情

--- deeptrace-search/src/main/java/com/qcl/service/EsAgentConfigService.java
+++ deeptrace-search/src/main/java/com/qcl/service/EsAgentConfigService.java
@@ -1,9 +1,11 @@
 package com.qcl.service;
 
 import com.qcl.entity.AgentConfig;
-
 import java.util.List;
 
 public interface EsAgentConfigService {
+    // 分页
     List<AgentConfig> search(String keyword, Integer pageNum, Integer pageSize);
+    // 单条
+    AgentConfig findByAgentLcuuid(String agentLcuuid);
 }
