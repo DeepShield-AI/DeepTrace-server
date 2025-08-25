17f244e9cf008d8814ccdebadc452e088ad10545
tbyno1 <m17864197141_1@163.com>
2025-08-25 09:52:11 +0800

    运行日志部分，因返回数据结构变化，api重新调整

--- deeptrace-search/src/main/java/com/qcl/entity/AgentLog.java
+++ deeptrace-search/src/main/java/com/qcl/entity/AgentLog.java
@@ -1,32 +1,38 @@
+// deeptrace-search/src/main/java/com/qcl/entity/AgentLog.java
 package com.qcl.entity;
 
-
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.io.Serializable;
-import java.util.LinkedHashMap;
 
-/**
- * 运行日志
- */
 @Data
 @EqualsAndHashCode
 @Document(indexName = "agent_log")
 public class AgentLog implements Serializable {
     private static final long serialVersionUID = -1L;
+
+    @Id
+    private String id; // ES的_id
+
     @Field(name = "agent_name")
     private String agentName;
 
-    @Id
+    @Field(name = "lcuuid")
     private String lcuuid;
 
-    @Field(type = FieldType.Object)
-    private Logs logs;
+    @Field(name = "content")
+    private String content;
+
+    @Field(name = "level")
+    private String level;
+
+    @Field(name = "timestamp")
+    private String timestamp;
+
+//    @Field(name = "logs")
+//    private Logs logs;
 }
