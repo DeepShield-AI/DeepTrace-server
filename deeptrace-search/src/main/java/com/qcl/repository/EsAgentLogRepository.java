6821af664cb2656772b22dc96dd44a6541fd3e39
tbyno1 <m17864197141_1@163.com>
2025-08-25 09:52:11 +0800

    运行日志部分，因返回数据结构变化，api重新调整

--- deeptrace-search/src/main/java/com/qcl/repository/EsAgentLogRepository.java
+++ deeptrace-search/src/main/java/com/qcl/repository/EsAgentLogRepository.java
@@ -8,5 +8,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EsAgentLogRepository extends ElasticsearchRepository<AgentLog, String> {
+    // 模糊查询 agentName 并分页
     Page<AgentLog> findByAgentNameContaining(String keyword, Pageable pageable);
+    // lcuuid 查询单条（返回第一条匹配对象，避免数组）
+    AgentLog findFirstByLcuuid(String lcuuid);
 }
