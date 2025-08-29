30f8b40c47d96ecd576ed2cc8092fbc5535b5783
tbyno1 <m17864197141_1@163.com>
2025-08-29 14:45:38 +0800

    调用链追踪api的深分页查询

--- deeptrace-search/src/main/java/com/qcl/service/EsTraceService.java
+++ deeptrace-search/src/main/java/com/qcl/service/EsTraceService.java
@@ -5,6 +5,7 @@ import com.qcl.entity.param.QueryTracesParam;
 import com.qcl.entity.statistic.LatencyTimeBucketResult;
 import com.qcl.entity.statistic.StatusTimeBucketResult;
 import com.qcl.entity.statistic.TimeBucketResult;
+import com.qcl.vo.PageResult;
 
 import java.util.List;
 import java.util.Map;
@@ -16,7 +17,11 @@ public interface EsTraceService {
     /**
      * 深分页查询 Trace 列表
      */
-    List<Traces> queryByPage(QueryTracesParam param);
+//    List<Traces> queryByPage(QueryTracesParam param);
+    /**
+     * 返回分页结果对象
+     */
+    PageResult<Traces> queryByPageResult(QueryTracesParam param);
 
     /**
      * 滚动查询 Trace 列表
