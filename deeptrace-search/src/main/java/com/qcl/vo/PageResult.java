650a5958bb169cf408b620d7232ea1e1eb03110c
tbyno1 <m17864197141_1@163.com>
2025-09-19 09:12:02 +0800

    优化公共的分页结果pageResult类

--- deeptrace-search/src/main/java/com/qcl/vo/PageResult.java
+++ deeptrace-search/src/main/java/com/qcl/vo/PageResult.java
@@ -1,11 +1,18 @@
 package com.qcl.vo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
 /**
  * 通用分页结果对象
  * @param <T> 分页内容类型
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PageResult<T> {
     /** 当前页内容列表 */
     private List<T> content;
@@ -14,22 +21,7 @@ public class PageResult<T> {
     private long totalElements;
     private int totalPages;
 
-    public PageResult(List<T> content, int pageNumber, int pageSize, long totalElements, int totalPages) {
-        this.content = content;
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
+    public static <T> PageResult<T> of(List<T> content, int pageNumber, int pageSize, long totalElements, int totalPages) {
+        return new PageResult<>(content, pageNumber, pageSize, totalElements, totalPages);
     }
-
-    public List<T> getContent() { return content; }
-    public void setContent(List<T> content) { this.content = content; }
-    public int getPageNumber() { return pageNumber; }
-    public void setPageNumber(int pageNumber) { this.pageNumber = pageNumber; }
-    public int getPageSize() { return pageSize; }
-    public void setPageSize(int pageSize) { this.pageSize = pageSize; }
-    public long getTotalElements() { return totalElements; }
-    public void setTotalElements(long totalElements) { this.totalElements = totalElements; }
-    public int getTotalPages() { return totalPages; }
-    public void setTotalPages(int totalPages) { this.totalPages = totalPages; }
 }
