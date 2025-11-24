609f974053836968e6b113f63766c23b17357841
tbyno1 <m17864197141_1@163.com>
2025-11-24 17:38:20 +0800

    整理优化部分 Trace api，优化公共pageResult类，增加单条trace查询，区分分页查询

--- deeptrace-search/src/main/java/com/qcl/vo/PageResult.java
+++ deeptrace-search/src/main/java/com/qcl/vo/PageResult.java
@@ -1,59 +1,26 @@
 package com.qcl.vo;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
 import java.util.List;
 
 /**
  * 通用分页结果对象
  * @param <T> 分页内容类型
  */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class PageResult<T> {
     private List<T> content;
     private int pageNumber;
     private int pageSize;
     private long totalElements;
     private int totalPages;
-    // 所有筛选项
-    private List<String> allEndpoints;
-    private List<String> allProtocols;
-    private List<String> allStatusOptions;
 
-    public PageResult(List<T> content, int pageNumber, int pageSize, long totalElements, int totalPages) {
-        this.content = content;
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-        this.totalPages = totalPages;
+    public static <T> PageResult<T> of(List<T> content, int pageNumber, int pageSize, long totalElements, int totalPages) {
+        return new PageResult<>(content, pageNumber, pageSize, totalElements, totalPages);
     }
-
-    public PageResult(List<T> content, int pageNumber, int pageSize, long totalElements) {
-        this.content = content;
-        this.pageNumber = pageNumber;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-        this.totalPages =  (int) Math.ceil((double) totalElements / pageSize);
-    }
-    public PageResult(List<T> content, int pageSize, long totalElements) {
-        this.content = content;
-        this.pageSize = pageSize;
-        this.totalElements = totalElements;
-        this.totalPages =  (int) Math.ceil((double) totalElements / pageSize);
-    }
-
-    public List<T> getContent() { return content; }
-    public void setContent(List<T> content) { this.content = content; }
-    public int getPageNumber() { return pageNumber; }
-    public void setPageNumber(int pageNumber) { this.pageNumber = pageNumber; }
-    public int getPageSize() { return pageSize; }
-    public void setPageSize(int pageSize) { this.pageSize = pageSize; }
-    public long getTotalElements() { return totalElements; }
-    public void setTotalElements(long totalElements) { this.totalElements = totalElements; }
-    public int getTotalPages() { return totalPages; }
-    public void setTotalPages(int totalPages) { this.totalPages = totalPages; }
-    // 所有筛选项的 getter和setter
-    public List<String> getAllEndpoints() { return allEndpoints; }
-    public void setAllEndpoints(List<String> allEndpoints) { this.allEndpoints = allEndpoints; }
-    public List<String> getAllProtocols() { return allProtocols; }
-    public void setAllProtocols(List<String> allProtocols) { this.allProtocols = allProtocols; }
-    public List<String> getAllStatusOptions() { return allStatusOptions; }
-    public void setAllStatusOptions(List<String> allStatusOptions) { this.allStatusOptions = allStatusOptions; }
 }
